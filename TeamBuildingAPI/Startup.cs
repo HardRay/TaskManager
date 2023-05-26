2bbcbfcd033af1ac18afd5515f40afd931731bd1
nepryakhin.aleksej <nepryahinalex@gmail.com>
2023-05-26 20:53:51 +0300

    TAS-4 Startup setup

---
+++ TeamBuildingAPI/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace TeamBuilding.Api
+{
+    public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers(options =>
+			{
+			}).AddNewtonsoftJson(options =>
+			{
+				options.SerializerSettings.ContractResolver = new DefaultContractResolver
+				{
+					NamingStrategy = new DefaultNamingStrategy()
+				};
+				options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+				options.SerializerSettings.Converters.Add(new StringEnumConverter());
+			}).ConfigureApiBehaviorOptions(options =>
+			{
+				options.SuppressModelStateInvalidFilter = true;
+			});
+
+			services.AddSwaggerGen();
+
+			services.AddAuthorization();
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IConfiguration configuration, ILoggerFactory loggerFactory)
+		{
+			app.Use(async (context, next) => {
+				context.Request.EnableBuffering();
+				await next();
+			});
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+
+			app.UseAuthorization();
+
+			app.UseSwagger();
+			app.UseSwaggerUI();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
