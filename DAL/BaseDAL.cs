b94bbd26e1dc34f09ed6e1244708c0da1a505249
nepryakhin.aleksej <nepryahinalex@gmail.com>
2023-05-26 20:38:04 +0300

    Remove files

--- DAL/BaseDAL.cs
+++ DAL/BaseDAL.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 
 namespace DAL
 {
--- DAL/UserDAL.cs
+++ DAL/UserDAL.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Entity;
+﻿using Entity;
 using System.Linq.Expressions;
 
 namespace DAL
